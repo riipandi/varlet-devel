d27f1de0af92c70e7755bb240eef6474313c44b4
Aris Ripandi <ar.is@outlook.com>
2019-12-04 01:25:45 +0700

    start moving to dotnet core

--- source/VarletCli/Program.cs
+++ source/VarletCli/Program.cs
@@ -1,61 +1,12 @@
-/**
- * Varlet Controller
- * 
- * The following code was provided for controlling Varlet. 
- * 
- * Author Name: Aris Ripandi <aris@ripandi.id>
- * Author Url: https://github.com/riipandi
- * 
- */
-
-using System;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using CommandLine;
-using Semver;
+﻿using System;
 
 namespace VarletCli
 {
-    static class Program
+    class Program
     {
-        private class Options
-        {
-            [Option('v', "verbose", Default = false, Required = false, HelpText = "Set output to verbose messages.")]
-            public bool Verbose { get; set; }
-        }
-        
         static void Main(string[] args)
         {
-            Helper hlp = new Helper();
-            var version = SemVersion.Parse("1.1.0");
-
-            Parser.Default.ParseArguments<Options>(args)
-                   .WithParsed<Options>(o =>
-                   {
-                       if (o.Verbose)
-                       {
-                           var table = new ConsoleTable("one", "two", "three");
-                           table.AddRow(1, 2, 3).AddRow("this line should be longer", "yes it is", "oh");
-
-                           Console.WriteLine("\nFORMAT: Default:\n");
-                           table.Write();
-
-                           Console.WriteLine("\nFORMAT: Alternative:\n");
-                           table.Write(Format.Alternative);
-                           Console.WriteLine();
-                       }
-                       else
-                       {
-                           var dirPath = Environment.CurrentDirectory;
-                           var dirName = Path.GetFileName(dirPath);
-                           hlp.PrintlnInfo("You are here: " + dirName.ToLower());
-                           hlp.PrintlnSuccess("Full path is: " + dirPath.Replace("\\", "/"));
-
-                           if (version >= "1.0")
-                               Console.WriteLine("You are using version {0}", version);
-                       }
-                   });
+            Console.WriteLine("Hello World!");
         }
     }
 }
--- source/VarletUi/Program.cs
+++ source/VarletUi/Program.cs
@@ -1,41 +1,23 @@
-/**
- * Varlet Controller
- *
- * The following code was provided for controlling Varlet.
- *
- * Author Name: Aris Ripandi <aris@ripandi.id>
- * Author Url: https://github.com/riipandi
- *
- */
-
 using System;
-using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace VarletUi
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            bool instanceCountOne = false;
-
-            using (Mutex mtex = new Mutex(true, "VarletUi", out instanceCountOne))
-            {
-                if (instanceCountOne) {
-                    Application.EnableVisualStyles();
-                    Application.SetCompatibleTextRenderingDefault(false);
-                    _ = new TrayContext();
-                    Application.Run(new FormMain());
-                    mtex.ReleaseMutex();
-                } else {
-                    MessageBox.Show(Application.ProductName + " already running!", "Warning");
-                }
-            }
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
         }
     }
 }
