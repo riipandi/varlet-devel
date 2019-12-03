0027d2ab7230daa6e5a8f5a4ba507ed9c6a96881
Aris Ripandi <ar.is@outlook.com>
2019-12-04 04:07:16 +0700

    add CommandLineParser and Fody Costura

--- source/VarletCli/Program.cs
+++ source/VarletCli/Program.cs
@@ -1,13 +1,44 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using Variety;
+using CommandLine;
 
 namespace VarletCli
 {
     class Program
     {
+        class Options
+        {
+            [Option('r', "read", Required = true, HelpText = "Input files to be processed.")]
+            public IEnumerable<string> InputFiles { get; set; }
+
+            // Omitting long name, defaults to name of property, ie "--verbose"
+            [Option(Default = false, HelpText = "Prints all messages to standard output.")]
+            public bool Verbose { get; set; }
+
+            [Option("stdin", Default = false, HelpText = "Read from stdin")]
+            public bool stdin { get; set; }
+
+            [Value(0, MetaName = "offset", HelpText = "File offset.")]
+            public long? Offset { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+              .WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts))
+              .WithNotParsed<Options>((errs) => HandleParseError(errs));
         }
+
+        private static void RunOptionsAndReturnExitCode(Options opts)
+        {
+            Console.WriteLine("handle: " + opts);
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errs)
+        {
+            Console.WriteLine("error: " + errs);
+        }
+
     }
 }
